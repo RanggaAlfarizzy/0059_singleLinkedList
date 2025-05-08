ca81ede417c0c43878f47d47cfa9084d2401c50f
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-08 20:20:04 +0800

    mencari node dengan nim tertentu

--- singleLinkList.cpp
+++ singleLinkList.cpp
@@ -63,3 +63,17 @@ void addNode()
         return (START == NULL);
     }
 
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return(*current != NULL);
+    }
+
