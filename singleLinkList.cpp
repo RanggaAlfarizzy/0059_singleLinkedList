f31751c1f0a2358fa0bf16b106b67c978bd94779
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-05-08 20:31:21 +0800

    Menyusun loop utama

--- singleLinkList.cpp
+++ singleLinkList.cpp
@@ -110,4 +110,21 @@ void addNode()
         }
 };
 
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+
+    do
+    {
+        cout << "\nMenu:" << endl;
+        cout << "1. Menambahkan data ke dalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data dalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+
 
